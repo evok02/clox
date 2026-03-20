fc03832fd58d77669d4a33b625b4dac1aba1a702
evok02 <hlibter1002@gmail.com>
2026-03-20 21:41:26 +0100

    added compiler

--- vm.c
+++ vm.c
@@ -96,6 +96,18 @@ static InterpretResult run() {
 }
 
 InterpretResult interpret(char* source) {
-    compile(source);
-    return INTERPRET_OK;
+    Chunk chunk;
+    initChunk(&chunk);
+
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+
+    InterpretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
 }
